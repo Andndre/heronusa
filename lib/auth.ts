3eee833490f5ca34fa37cbb218c3f33a37b80e5b
Andndre <agungandre687@gmail.com>
2026-02-13 08:39:50 +0800

    feat: update .gitignore and docker-compose for MySQL container name; enhance auth-client and auth with organization support; extend Prisma schema with new models for organization, team, member, and invitation

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
 import { nextCookies } from "better-auth/next-js";
+import { organization } from "better-auth/plugins";
 
 export const auth = betterAuth({
   emailAndPassword: {
@@ -10,5 +11,12 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "mysql",
   }),
-  plugins: [nextCookies()],
+  plugins: [
+    nextCookies(),
+    organization({
+      teams: {
+        enabled: true,
+      },
+    }),
+  ],
 });
