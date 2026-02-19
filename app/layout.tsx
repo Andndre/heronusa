3d7bb3da362a0165051754ec09ed169ba36a859b
Andndre <agungandre687@gmail.com>
2026-02-19 21:43:10 +0800

    fix(layout): set showSpinner to false for NextjsTopLoader component

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <NextjsTopLoader color="#e21c21" />
+          <NextjsTopLoader color="#e21c21" showSpinner={false} />
           {children}
           <Toaster />
         </ThemeProvider>
